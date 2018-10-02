7c2df7e47b867b935f9cd97c118da8180a0610d5
Marco Dagrada <marco.dagrada@telnext.com>
2018-10-02 08:51:56 +0200

    Server configuration

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 // server.js
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-const port = process.env.PORT || 5000;
-app.listen(port);
+const express = nrequire('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(serveStatic(__dirname + "/dist"))
+
+app.use((err, request, response, next) => {
+  // log the error, by now it's just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+  next()
+})
+
+app.listen(port)
 console.log(`Server is up & running on port ${port}`)
